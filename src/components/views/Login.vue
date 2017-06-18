c57c74e1573eccd7c6afd06cedb977a27643c774
papaya26 <juddisgood999@gmail.com>
2017-06-18 23:49:27 +0800

    login page form

--- src/components/views/Login.vue
+++ src/components/views/Login.vue
@@ -1,21 +1,34 @@
 <template>
-<!-- root node required -->
-<div class="login">
-  <div class="row card-container justify-center items-center">
-    <div class="card-title bg-primary text-white sm-width-4of5 md-width-3of5 gt-md-width-2of5">
-      Colored Card Title
+  <!-- root node required -->
+  <div class="login">
+    <div class="row card-container justify-center items-center">
+      <div class="sm-width-4of5 md-width-3of5 gt-md-width-2of5">
+        <div class="card">
+          <div class="card-title bg-primary text-white">
+            Login
+          </div>
+          <div class="card-content bg-light card-force-top-padding">
+            <form>
+              <div class="floating-label">
+                <input required class="full-width" v-model="loginForm.username">
+                <label>Username</label>
+              </div>
+              <div class="floating-label">
+                <input required class="full-width" v-model="loginForm.password">
+                <label>Password</label>
+              </div>
+              <div class="button-container">
+                <button class="primary round" v-on:click.prevent="login()">
+                  Login
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
-</div>
 </template>
-
-<script>
-export default {
-  data () {
-    return {}
-  }
-}
-</script>
 
 <style>
 .login {
@@ -25,4 +38,27 @@ export default {
 .card-container {
   height: 100%;
 }
+
+.button-container {
+  margin: 10px 0;
+  text-align: center; 
+}
 </style>
+
+<script>
+  export default {
+    data () {
+      return {
+        loginForm: {
+          username: '',
+          password: ''
+        }
+      }
+    },
+    methods: {
+      login () {
+        console.log('loginForm values: ' + JSON.stringify(this.loginForm))
+      }
+    }
+  }
+</script>
