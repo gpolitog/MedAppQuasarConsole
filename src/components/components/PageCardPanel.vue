7b90121b62b967b6ab91db17fc0822dba9667595
papaya26 <juddisgood999@gmail.com>
2017-06-27 22:00:55 +0800

    implemented page card panel header and body slots. partial accounts page

--- src/components/components/PageCardPanel.vue
+++ src/components/components/PageCardPanel.vue
@@ -2,13 +2,21 @@
     <div class="height-90 sm-width-4of5 md-width-4of5 gt-md-width-4of5">
         <div class="card">
             <div class="card-content bg-light">
-                <slot></slot>
+                <div class="row card-header">
+                    <slot name="left-header"></slot>
+                    <slot name="right-header"></slot>
+                </div>
+    
+                <div class="card-body">
+                    <slot name="body"></slot>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
-<style scoped>
+<style scoped lang="stylus">
+@import '~src/themes/app.variables.styl';
 .height-90 {
     height: 90%;
 }
@@ -19,6 +27,18 @@
 
 .card-content {
     height: 100%;
+}
+
+.card-header {
+    justify-content: space-between;
+    align-content: center;
+    align-items: center;
+    border-bottom: 1px solid black;
+    font-size: $page-card-panel-header;
+}
+
+.card-body {
+    margin: 10px;
 }
 </style>
 
--- src/components/views/Accounts.vue
+++ src/components/views/Accounts.vue
@@ -1,38 +1,60 @@
 <template>
     <page-content>
         <page-card-panel>
-            <div class="row page-header">
-                <i>group</i>
-                <span>Accounts</span>
+            <div slot="left-header">
+                <i>group</i> Accounts
+            </div>
+            <div slot="right-header">
+                <button class="primary clear" v-on:click.prevent="createAccount()">
+                    <i>add_circle_outline</i>
+                </button>
+            </div>
+    
+            <div slot="body">
+                <div class="row">
+                    <div class="sm-width-5of5 md-width-1of3 gt-md-width-1of3">
+                        <q-search :debounce="1000" v-model="username" placeholder="Username" @input="search()"></q-search>
+                    </div>
+                </div>
+    
+                <div class="row account-list">
+                    <div class="sm-width-5of5 md-width-1of3 gt-md-width-1of3">
+                        accounts table here
+                    </div>
+                </div>
             </div>
         </page-card-panel>
     </page-content>
 </template>
 
+<style scoped>
+.account-list {
+    margin-top: 20px;
+}
+</style>
+
 <script>
 import pageCardPanel from '../components/PageCardPanel.vue'
 import pageContent from '../components/PageContent.vue'
+
 export default {
     components: {
         pageCardPanel,
         pageContent
+    },
+    data() {
+        return {
+            username: ''
+        }
+    },
+    methods: {
+        createAccount() {
+            console.log('create account');
+        },
+        search() {
+            console.log('searched => ' + this.username);
+        }
     }
 }
 </script>
 
-<style lang="stylus">
-@import '~src/themes/app.variables.styl';
-
-.page-header {
-    border-bottom: 1px solid black;
-}
-
-i {
-    font-size: $page-card-panel-header;
-}
-
-span {
-    font-size: $page-card-panel-header;
-    margin-left: 10px;
-}
-</style>
