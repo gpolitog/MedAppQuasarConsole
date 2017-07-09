2895b74bf373e5527be2f066c1b39a044ba4ea45
papaya26 <juddisgood999@gmail.com>
2017-07-10 00:21:53 +0800

    idling

--- src/main.js
+++ src/main.js
@@ -8,15 +8,17 @@ require(`quasar/dist/quasar.${__THEME}.css`)
 
 import Vue from 'vue'
 import Quasar from 'quasar'
+import IdleVue from 'idle-vue'
 import Vuelidate from 'vuelidate'
 
 import router from './config/router'
 import store from './config/store'
 
 import UTILS from './utils/utils'
+import STORAGE from './utils/storage'
 
-Vue.use(Quasar)
 Vue.use(Vuelidate)
+Vue.use(Quasar)
 
 // nav guard
 router.beforeEach((to, from, next) => {
@@ -32,12 +34,29 @@ router.beforeEach((to, from, next) => {
   }
 })
 
+// idling - 2mins in ms
+const eventsHub = new Vue()
+
+Vue.use(IdleVue, {
+  eventEmitter: eventsHub,
+  idleTime: 120000
+})
+
 Quasar.start(() => {
   /* eslint-disable no-new */
   new Vue({
     el: '#q-app',
     router,
     store,
-    render: h => h(require('./App'))
+    render: h => h(require('./App')),
+    onIdle() {
+      console.log(`auto logged out. console is inactive.`)
+      store.dispatch('loggedIn', false)
+      router.push('/')
+      STORAGE.clear()
+    },
+    onActive() {
+      console.log(`console is active`)
+    }
   })
 })
