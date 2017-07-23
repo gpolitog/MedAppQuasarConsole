dc42f87b0d5ff3f013f7b4fa82b7e8b0b07f5689
papaya26 <juddisgood999@gmail.com>
2017-07-23 23:19:35 +0800

    implemented numeric validator

--- src/components/views/Accounts.vue
+++ src/components/views/Accounts.vue
@@ -48,6 +48,7 @@
                     <input type="number" class="full-width" :disabled="isProcessing" v-model="newAccount.noOfClinic" @keypress="isNumber(event)">
                     <label>Number Of Clinics</label>
                     <p class="error-msg" v-if="$v.newAccount.noOfClinic.$error && !$v.newAccount.noOfClinic.required">Number Of Clinics is required!</p>
+                    <p class="error-msg" v-if="$$v.newAccount.noOfClinic.$error && !$$v.newAccount.noOfClinic.numeric">Number Of Clinics is not a number!</p>
                 </div>
             </form>
         </modal-component>
@@ -198,7 +199,7 @@ export default {
     validations: {
         newAccount: {
             username: { required, email },
-            noOfClinic: { required }
+            noOfClinic: { required, numeric }
         },
         noOfClinic: { required, numeric }
     },
